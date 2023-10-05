146e00c514437532c0274719e90168f83d9c9bda
alcarazaina <alcarazdainara@gmail.com>
2023-10-05 17:14:41 +0200

    Actualización

---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosIf/Ejercicio03.java
@@ -0,0 +1,50 @@
+package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosIf;
+
+import java.util.Scanner;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+        /*
+        Escribe un programa que pide 5 números por teclado y al concluir escribe el mayor y menor de ellos (sin utilizar for).
+         */
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce 5 números: ");
+        int num1 = teclado.nextInt();
+        int num2 = teclado.nextInt();
+        int num3 = teclado.nextInt();
+        int num4 = teclado.nextInt();
+        int num5 = teclado.nextInt();
+
+        int mayorTemporal = num1;
+        if (mayorTemporal < num2) {
+            mayorTemporal = num2;
+        }
+        if (mayorTemporal < num3) {
+            mayorTemporal = num3;
+        }
+        if (mayorTemporal < num4) {
+            mayorTemporal = num4;
+        }
+        if (mayorTemporal < num5) {
+            mayorTemporal = num5;
+        }
+
+        int menorTemporal = num1;
+        if (menorTemporal > num2) {
+            menorTemporal = num2;
+        }
+        if (menorTemporal > num3) {
+            menorTemporal = num3;
+        }
+        if (menorTemporal > num4) {
+            menorTemporal = num4;
+        }
+        if (menorTemporal > num5) {
+            menorTemporal = num5;
+        }
+
+        System.out.println("El número mayor es: " + mayorTemporal + " y el menor es: " + menorTemporal);
+
+
+    }
+}
