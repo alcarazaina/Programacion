fb733f28ab552df49e69d8c12f3afcf8b9d0f6f0
alcarazaina <alcarazdainara@gmail.com>
2023-10-09 16:55:37 +0200

    Actualización

--- src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosIf/Ejercicio01.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosIf/Ejercicio01.java
@@ -8,12 +8,12 @@ public class Ejercicio01 {
         Escribe un programa que pida un número e indique si es par o impar (un número es par si al dividirlo entre 2 el resto es 0)
          */
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Introduce un número par o impar: ");
+        System.out.println("Introduce un número entero; par o impar: ");
         int numero = teclado.nextInt();
         if (numero % 2 == 0) {
-            System.out.println("El número es par ");}
+            System.out.println("El número " + numero + " es par ");}
             else {
-                System.out.println("El número es impar ");
+                System.out.println("El número " + numero + " es impar ");
             }
     }
 }
