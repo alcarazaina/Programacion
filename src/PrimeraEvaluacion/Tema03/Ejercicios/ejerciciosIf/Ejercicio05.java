5d6c89997f5915e47a728d79e9088c3b338637d8
alcarazaina <alcarazdainara@gmail.com>
2023-10-09 17:38:12 +0200

    Actualización

--- src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosIf/Ejercicio05.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosIf/Ejercicio05.java
@@ -6,12 +6,12 @@ public class Ejercicio05 {
     public static void main(String[] args) {
         /*
         Escribe un programa que pida un nombre de usuario y una contraseña. Esos datos se comparan con dos valores guardados previamente en el programa. Si coinciden, se indica “Has entrado al sistema”, en caso contrario se indica que usuario y/o contraseña son incorrectos.
-Escribe el mismo programa anterior, pero indicando exactamente qué es lo que es incorrecto: si el usuario, la contraseña o ambos.
-
-         */
+        Escribe el mismo programa anterior, pero indicando exactamente qué es lo que es incorrecto: si el usuario, la contraseña o ambos.
+        */
 
         String user = "ainara26";
         String password = "Fernando6";
+
         Scanner teclado = new Scanner(System.in);
         System.out.println("Usuario: ");
         String usuario = teclado.nextLine();
@@ -20,8 +20,13 @@ Escribe el mismo programa anterior, pero indicando exactamente qué es lo que es
 
         if (contraseña.equals(password) && usuario.equals(user)) {
             System.out.println("Has entrado al sistema");
-        } else {
-            System.out.println("Error de credenciales");
+        } else if (!contraseña.equals(password) && usuario.equals(user)){
+            System.out.println("Contraseña incorrecta");
+        } else if (contraseña.equals(password) && !usuario.equals(user)) {
+            System.out.println("Usuario incorrecto");
+        }else {
+                System.out.println( "Usuario y contraseña incorrectos");
+            }
+
         }
     }
-}
