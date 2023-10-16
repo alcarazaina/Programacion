c1f933cd75cd8b80c87bde8f10bdf117b16bb905
alcarazaina <alcarazdainara@gmail.com>
2023-10-16 17:46:32 +0200

    Actualización

--- src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosfor/Ejercicio22.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosfor/Ejercicio22.java
@@ -9,7 +9,15 @@ public class Ejercicio22 {
 Hacer un programa que genere 10 números aleatoriamente y calcule su media.
  */
         Random generator = new Random();
-        int numerosAleatorios = generator.nextInt();
-        int suma = numerosAleatorios * numerosAleatorios;
+        int numerosAleatorios = generator.nextInt(1,11);
+        int suma = 0;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(numerosAleatorios);
+            numerosAleatorios = generator.nextInt(1,11);
+        suma = suma + numerosAleatorios;
+        }
+        System.out.println("La suma es: " + suma);
+        int media = suma / 10;
+        System.out.println("La media es " + media);
     }
 }
