f093ab31321ac6f60d8247e7b9ec1aff9fb53ae9
alcarazaina <alcarazdainara@gmail.com>
2023-10-18 19:18:14 +0200

    Actualización

---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosfor/Ejercicio27.java
@@ -0,0 +1,13 @@
+package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosfor;
+
+import java.util.Scanner;
+
+public class Ejercicio27 {
+    public static void main(String[] args) {
+        /*
+        Hacer un programa que calcule el factorial de un número pedido por teclado. (Ejemplo: el factorial de 5 sería 5 * 4 * 3 * 2 * 1; el de 4 sería 4* 3 * 2 * 1, etc.)
+         */
+        Scanner teclado = new Scanner(System.in);
+
+    }
+}
