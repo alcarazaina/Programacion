3012694d565bf368a4435199df24ab2267ef0743
alcarazaina <alcarazdainara@gmail.com>
2023-10-19 16:02:07 +0200

    Actualización

---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosfor/Ejercicio29.java
@@ -0,0 +1,9 @@
+package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosfor;
+
+public class Ejercicio29 {
+    public static void main(String[] args) {
+        /*
+        Escribe un programa que, dados dos números, uno real (base) y un entero positivo (exponente), saque por pantalla el resultado de la potencia. No se puede utilizar el método  Math.pow().
+         */
+    }
+}
