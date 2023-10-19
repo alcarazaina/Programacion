c988bb0baa8ddf94de815592809baa480d8df028
alcarazaina <alcarazdainara@gmail.com>
2023-10-19 16:33:54 +0200

    Actualización

--- src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosfor/Ejercicio29.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosfor/Ejercicio29.java
@@ -1,9 +1,19 @@
 package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosfor;
 
+import java.util.Scanner;
+
 public class Ejercicio29 {
     public static void main(String[] args) {
         /*
         Escribe un programa que, dados dos números, uno real (base) y un entero positivo (exponente), saque por pantalla el resultado de la potencia. No se puede utilizar el método  Math.pow().
          */
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un número y su potencia: ");
+        int numero = teclado.nextInt();
+        int potencia = teclado.nextInt();
+        int resultado = 0;
+        for (int i = numero; i <= potencia; resultado =  numero * numero) {
+            System.out.println(numero);
+        }
     }
 }
