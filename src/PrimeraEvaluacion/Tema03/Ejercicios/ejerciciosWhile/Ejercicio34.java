cc7f308b6e17046f7239f57a843fa0fc187b8edc
alcarazaina <alcarazdainara@gmail.com>
2023-10-21 12:20:43 +0200

    Actualización

---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosWhile/Ejercicio34.java
@@ -0,0 +1,9 @@
+package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosWhile;
+
+public class Ejercicio34 {
+    public static void main(String[] args) {
+        /*
+        Dada una secuencia de números enteros (que finaliza con el número 0), encuentra el elemento más grande de la secuencia. El número 0 no está incuido en la secuencia, sólo se utiliza para finalizar el programa.
+         */
+    }
+}
