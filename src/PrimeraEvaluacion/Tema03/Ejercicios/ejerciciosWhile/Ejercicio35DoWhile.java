a7d7573efefe944b5b556d044b5555244a3de306
alcarazaina <alcarazdainara@gmail.com>
2023-10-23 17:47:41 +0200

    Actualización

---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosWhile/Ejercicio35DoWhile.java
@@ -0,0 +1,22 @@
+package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosWhile;
+
+import java.util.Scanner;
+
+public class Ejercicio35DoWhile {
+    public static void main(String[] args) {
+         /*
+        Dada una secuencia de números enteros introducidos por teclado (que finaliza con el número 0), encuentra el elemento más grande de la secuencia. El número 0 no está incuido en la secuencia, sólo se utiliza para finalizar el programa.
+         */
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce una serie de números, y 0 para finalizar: ");
+        int numeros = teclado.nextInt();
+        int numeromayor = 0;
+        do {
+            numeros = teclado.nextInt();
+            if (numeros > numeromayor){
+                numeromayor = numeros;}
+        }while (numeros != 0);
+
+        System.out.println(numeromayor);
+    }
+}
---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosWhile/Ejercicio37.java
@@ -0,0 +1,10 @@
+package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosWhile;
+
+public class Ejercicio37 {
+    public static void main(String[] args) {
+        /*
+        Hacer un programa que imprima por pantalla los 50 primeros múltiplos de 5, añadiendo un salto de línea cada 10 números.
+         */
+
+    }
+}
