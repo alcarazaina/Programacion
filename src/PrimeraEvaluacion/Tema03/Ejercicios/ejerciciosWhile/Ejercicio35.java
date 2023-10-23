b686f11bad5920b9fe4a4531ec0b055db5525188
alcarazaina <alcarazdainara@gmail.com>
2023-10-23 17:42:16 +0200

    Actualización

--- src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosWhile/Ejercicio35.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosWhile/Ejercicio35.java
@@ -5,17 +5,15 @@ import java.util.Scanner;
 public class Ejercicio35 {
     public static void main(String[] args) {
         /*
-        Dada una secuencia de números enteros (que finaliza con el número 0), encuentra el elemento más grande de la secuencia. El número 0 no está incuido en la secuencia, sólo se utiliza para finalizar el programa.
+        Dada una secuencia de números enteros introducidos por teclado (que finaliza con el número 0), encuentra el elemento más grande de la secuencia. El número 0 no está incuido en la secuencia, sólo se utiliza para finalizar el programa.
          */
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce una serie de números, y 0 para finalizar: ");
         int numeros = teclado.nextInt();
-        int mayor = 0;
         int numeromayor = 0;
         while (numeros != 0){
             numeros = teclado.nextInt();
-            numeros = mayor;
-            if (mayor < numeros){
+            if (numeros > numeromayor){
                 numeromayor = numeros;
             }
         }
