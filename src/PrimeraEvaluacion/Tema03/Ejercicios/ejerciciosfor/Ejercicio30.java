9707e7559bab7ae0b166b83b74cc9c42a6f1c986
alcarazaina <alcarazdainara@gmail.com>
2023-10-25 23:11:57 +0200

    Actualización

---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosfor/Ejercicio30.java
@@ -0,0 +1,9 @@
+package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosfor;
+
+public class Ejercicio30 {
+    public static void main(String[] args) {
+        /*
+        Realizar un programa que pida números (al arrancar el programa se pedirá por teclado la cantidad de números a introducir). El programa debe informar de cuántos números introducidos son mayores que 0, menores que 0 e iguales a 0
+         */
+    }
+}
