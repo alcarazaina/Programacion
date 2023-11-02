601b9834cd985a9b403f6584b734ccadb78eddff
alcarazaina <alcarazdainara@gmail.com>
2023-11-02 16:46:08 +0100

    Actualización

---
+++ src/PrimeraEvaluacion/Tema04/Ejercicios/String/Ejercicio01.java
@@ -0,0 +1,21 @@
+package PrimeraEvaluacion.Tema04.Ejercicios.String;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        /*
+        Escribir por pantalla cada carácter de una cadena introducida por teclado.
+         */
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce una cadena de texto: ");
+        String cadena = teclado.nextLine();
+
+        // para verla carácter a carácter -> charAt(indice)
+
+        for (int i = 0; i < cadena.length(); i++) { //
+            System.out.println(cadena.charAt(i));
+
+        }
+    }
+}
