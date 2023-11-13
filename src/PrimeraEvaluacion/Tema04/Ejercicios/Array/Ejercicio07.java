e8966dab37f24c654a503aec9dc060fe34f5e5ec
alcarazaina <alcarazdainara@gmail.com>
2023-11-13 18:03:50 +0100

    Actualización

---
+++ src/PrimeraEvaluacion/Tema04/Ejercicios/Array/Ejercicio07.java
@@ -0,0 +1,9 @@
+package PrimeraEvaluacion.Tema04.Ejercicios.Array;
+
+public class Ejercicio07 {
+    public static void main(String[] args) {
+        /*
+        Se quiere realizar un programa que lea por teclado las 5 notas obtenidas por un alumno (comprendidas entre 0 y 10). A continuación debe mostrar todas las notas, la nota media, la nota más alta que ha sacado y la menor
+         */
+    }
+}
