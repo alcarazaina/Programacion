547ae86cf81d16f8de17da5674684e9a47206f9b
alcarazaina <alcarazdainara@gmail.com>
2023-11-16 16:53:03 +0100

    Actualización

---
+++ src/PrimeraEvaluacion/Tema04/Ejercicios/Array/Bidimensionales/Ejercicio19.java
@@ -0,0 +1,27 @@
+package PrimeraEvaluacion.Tema04.Ejercicios.Array.Bidimensionales;
+
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+public class Ejercicio19 {
+    public static void main(String[] args) {
+        /*
+        Haz un programa que realice lo siguiente:
+Crea un array bidimensional de 5x5 enteros y rellénalo con valores numéricos.
+Suma todos los elementos de cada fila y todos los elementos de cada columna visualizando los resultados en pantalla
+         */
+        int[][] caracteres = new int[5][5];
+        Random generador = new Random();
+        for (int i = 0; i < caracteres.length; i++) {
+            for (int j = 0; j < caracteres[i].length; j++) {
+                caracteres[i][j] = generador.nextInt(1,11);
+            }
+        }
+        for (int i = 0; i < caracteres.length; i++) {
+            for (int j = 0; j < caracteres[i].length; j++) {
+                System.out.print(caracteres[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
