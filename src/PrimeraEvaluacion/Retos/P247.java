71096f6adf9fc5012b4617ebf8cdb995a49c31d2
alcarazaina <alcarazdainara@gmail.com>
2023-11-16 16:00:31 +0100

    Actualización

--- src/PrimeraEvaluacion/Retos/P247.java
+++ src/PrimeraEvaluacion/Retos/P247.java
@@ -14,10 +14,28 @@ public class P247 {
         while (n != 0) {
             String linea = teclado.nextLine();
             String[] elementosDeLaLinea = linea.split(" ");
-            System.out.println(Arrays.toString(elementosDeLaLinea));
-            n = teclado.nextInt();
-        }
+            //System.out.println(Arrays.toString(elementosDeLaLinea));
 
+            int[] numeros = new int[elementosDeLaLinea.length];
+            for (int i = 0; i < n; i++) {
+                numeros[i] = Integer.parseInt(elementosDeLaLinea[i]);
+            }
+            boolean salimosDeLaCrisis = true;
+            for (int i = 0; i < n - 1; i++) { // el - 1 es para no salirme del array cuando llegue a la penúltima posición
+                if (numeros[i] >= numeros[i + 1]) {
+                    salimosDeLaCrisis = false;
+                    break;
+                }
+            }
+            if (salimosDeLaCrisis) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+        }
+        n = teclado.nextInt();
+        teclado.nextLine();
+
+    }
         teclado.close();
     }
 }
