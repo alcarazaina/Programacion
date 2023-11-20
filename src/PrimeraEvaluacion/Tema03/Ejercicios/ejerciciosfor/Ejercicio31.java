c76db1ab28c36c8ccfbe1f4a766a9500318ec1be
alcarazaina <alcarazdainara@gmail.com>
2023-11-20 17:16:38 +0100

    Actualización

---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/ejerciciosfor/Ejercicio31.java
@@ -0,0 +1,25 @@
+package PrimeraEvaluacion.Tema03.Ejercicios.ejerciciosfor;
+
+import java.util.Scanner;
+
+public class Ejercicio31 {
+    public static void main(String[] args) {
+        /*
+        [FOR ANIDADO] Hacer un programa que dibuje un triángulo rectángulo de n elementos de lado, siendo n un número introducido por teclado, utilizando asteriscos (*). Por ejemplo, para n = 5:
+*****
+****
+***
+*
+
+         */
+        Scanner teclado = new Scanner(System.in);
+        int n = teclado.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
