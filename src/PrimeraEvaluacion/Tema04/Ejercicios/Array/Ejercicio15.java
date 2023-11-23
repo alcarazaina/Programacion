0704439a8f68b039c51e4961c4b941faa0d019f4
alcarazaina <alcarazdainara@gmail.com>
2023-11-23 17:28:39 +0100

    Actualización

--- src/PrimeraEvaluacion/Tema04/Ejercicios/Array/Ejercicio15.java
+++ src/PrimeraEvaluacion/Tema04/Ejercicios/Array/Ejercicio15.java
@@ -1,10 +1,16 @@
 package PrimeraEvaluacion.Tema04.Ejercicios.Array;
 
+import java.util.Random;
+
 public class Ejercicio15 {
     public static void main(String[] args) {
         /*
         Crear un programa que rellene aleatoriamente un array de 10 números enteros. Guardar en otro array los elementos pares del primero, y, a continuación, los elementos impares
          */
-
+        Random generador = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array [i] = generador.nextInt(1,10);
+        }
     }
 }
