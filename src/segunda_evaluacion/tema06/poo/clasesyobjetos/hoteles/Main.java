a29999f1ec3c61ea44bee45391fabf2a68bfe8db
alcarazaina <alcarazdainara@gmail.com>
2024-01-10 17:31:57 +0100

    Actualización

---
+++ src/segunda_evaluacion/tema06/poo/clasesyobjetos/hoteles/Main.java
@@ -0,0 +1,13 @@
+package segunda_evaluacion.tema06.poo.clasesyobjetos.hoteles;
+
+public class Main {
+    public static void main(String[] args) {
+    Hotel hotel1 = new Hotel(1, 400, Zona.PLAYA);
+        System.out.println(hotel1);
+        hotel1.setPrecio(600);
+        System.out.println(hotel1);
+        hotel1.setPrecio(60);
+        System.out.println(hotel1);
+        hotel1.setPrecio(36);
+    }
+}
