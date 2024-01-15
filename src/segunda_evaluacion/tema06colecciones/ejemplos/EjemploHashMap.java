2006e0a5dd88525b1c77ba05032df2e26eba0129
alcarazaina <alcarazdainara@gmail.com>
2024-01-15 17:23:43 +0100

    Actualización

--- src/segunda_evaluacion/tema06colecciones/ejemplos/EjemploHashMap.java
+++ src/segunda_evaluacion/tema06colecciones/ejemplos/EjemploHashMap.java
@@ -24,5 +24,40 @@ public class EjemploHashMap {
         System.out.println(mapa.get(dni));
 
 
+        // Vamos a hacer un mapa que me guarde los numeros en inglés
+
+        HashMap<Integer, String> numeroIng = new HashMap<>();
+        numeroIng.put(1, "One");
+        numeroIng.put(2, "Two");
+        numeroIng.put(3, "Three");
+        numeroIng.put(4, "Four");
+        numeroIng.put(5, "Five");
+        numeroIng.put(6, "Six");
+        numeroIng.put(7, "Seven");
+        numeroIng.put(8, "Eight");
+        numeroIng.put(9, "Nine");
+        numeroIng.put(10, "Ten");
+
+
+        int numero = 0;
+        do {
+            System.out.println("¿Cómo se dice en inglés? 1 - 10 (0 para salir)");
+            numero = teclado.nextInt(); teclado.nextLine();
+            // recupero la traducción, si está, en el mapa y la imrpimo
+            if (numeroIng.containsKey(numero)) {
+            System.out.println(numeroIng.get(numero));
+            } else {
+                if (numero != 0) {
+                    System.out.println("Ese no me la sé. ¿Quieres enseñarmelo?\nDime la traducción: ");
+                    String traduccion = teclado.nextLine();
+                    numeroIng.put(numero, traduccion);
+                }
+            }
+        } while (numero != 0);
+
+
+
+
+
     }
 }
