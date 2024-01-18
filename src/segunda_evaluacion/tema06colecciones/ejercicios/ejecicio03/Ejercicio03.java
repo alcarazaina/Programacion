18cd550f2bb7a8f27ada08be1071e7d8937225ed
alcarazaina <alcarazdainara@gmail.com>
2024-01-18 15:56:55 +0100

    Actualización

--- src/segunda_evaluacion/tema06colecciones/ejercicios/ejecicio03/Ejercicio03.java
+++ src/segunda_evaluacion/tema06colecciones/ejercicios/ejecicio03/Ejercicio03.java
@@ -9,7 +9,7 @@ public class Ejercicio03 {
         /*
           Escribir un programa que cree un diccionario de traducción español-inglés. El usuario introducirá las palabras en español e inglés separadas por dos puntos, y cada par <palabra>:<traducción> separados por comas. El programa debe crear un diccionario con las palabras y sus traducciones. Después pedirá una frase en español y utilizará el diccionario para traducirla palabra a palabra. Si una palabra no está en el diccionario debe dejarla sin traducir.
          */
-        // perro:dog,gato:cat,mesa:table,silla:chair,clase:class,conseguir:get,establecer:set
+        // perro:dog,gato:cat,mesa:table,silla:chair,clase:class,conseguir:get,establecer:set,encima:on
         Scanner teclado = new Scanner(System.in);
         String linea = teclado.nextLine();
         System.out.println(linea);
@@ -26,5 +26,20 @@ public class Ejercicio03 {
             traductor.put(español, inglés);
         }
         System.out.println(traductor);
+        // Pedimos una frase en español y utiliza el diccionario para traducirla. Si la palabra no está te la deja en español.
+        // El perro está encima de la mesa
+        System.out.println("Introduce la frase a traducir: ");
+        // reutilizamos la variable línea
+        linea = teclado.nextLine();
+        String [] palabrasFrase = linea.split(" "); // creo un array con las palabras de la frase
+        // recorremos este array de palabras para ir traduciendolas una a una
+        for (String palabraEspañol: palabrasFrase) {
+            if (traductor.containsKey(palabraEspañol)){
+                System.out.print(traductor.get(palabraEspañol) + " "); // palabraEspañol es la clave (key) para obtener su traduccioón con get(palabraEspañol)
+            } else {
+                System.out.print(palabraEspañol + " "); // si no está la palabra en el mapa que me la imprima tal cual
+            }
+        }
+
     }
 }
