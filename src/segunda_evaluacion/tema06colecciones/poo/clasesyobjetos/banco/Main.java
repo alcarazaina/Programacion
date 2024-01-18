cc2d66031a7d507864b9ef143ae8b19315bf2782
alcarazaina <alcarazdainara@gmail.com>
2024-01-18 16:33:16 +0100

    Actualización

--- src/segunda_evaluacion/tema06colecciones/poo/clasesyobjetos/banco/Main.java
+++ src/segunda_evaluacion/tema06colecciones/poo/clasesyobjetos/banco/Main.java
@@ -3,5 +3,15 @@ package segunda_evaluacion.tema06colecciones.poo.clasesyobjetos.banco;
 public class Main {
     public static void main(String[] args) {
 
+        Usuario propietario = new Usuario("demo","Ale","Chim Pom");
+        System.out.println(propietario);
+
+        Cuenta cuenta = new Cuenta("123456",1000,propietario);
+
+        mostrarCuenta(cuenta);
+
+    }
+    public static void mostrarCuenta (Cuenta cuenta){
+        System.out.println(cuenta);
     }
 }
