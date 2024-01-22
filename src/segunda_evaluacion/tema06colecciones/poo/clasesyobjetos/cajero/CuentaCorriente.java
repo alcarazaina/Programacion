2cf7241a7506906c0166ae16443732019a49f31a
alcarazaina <alcarazdainara@gmail.com>
2024-01-22 17:55:16 +0100

    Actualización

---
+++ src/segunda_evaluacion/tema06colecciones/poo/clasesyobjetos/cajero/CuentaCorriente.java
@@ -0,0 +1,53 @@
+package segunda_evaluacion.tema06colecciones.poo.clasesyobjetos.cajero;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class CuentaCorriente {
+
+    private String dni;
+    private String nombre;
+    private double saldo;
+
+
+    public CuentaCorriente(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.saldo = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaCorriente{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
+
+    public void sacarDinero(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Cantidad a retirar: ");
+        int retirada = teclado.nextInt();
+        if (saldo >= retirada){
+            saldo = saldo - retirada; teclado.nextLine();
+            System.out.println("Te quedan " + saldo + " euros en la cuenta.");
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public void ingresarDinero(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Cantidad a ingresar: ");
+        double ingreso = teclado.nextDouble();
+        saldo = saldo + ingreso;
+        System.out.println("Nuevo saldo: " + saldo);
+
+    }
+
+    public void actualizarLibreta(){
+        System.out.println("Titular: " + nombre + "\nsaldo: " + saldo + "\n *** Libreta actualizada ***");
+    }
+
+}
---
+++ src/segunda_evaluacion/tema06colecciones/poo/clasesyobjetos/cajero/Main.java
@@ -0,0 +1,13 @@
+package segunda_evaluacion.tema06colecciones.poo.clasesyobjetos.cajero;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        CuentaCorriente cuenta1 = new CuentaCorriente("12345678A", "Perico Palote");
+        System.out.println(cuenta1);
+        cuenta1.ingresarDinero();
+        cuenta1.sacarDinero();
+        cuenta1.actualizarLibreta();
+    }
+}
