765c25ea699f753869ef78ca11c95b3b956eccaf
alcarazaina <alcarazdainara@gmail.com>
2024-01-29 17:56:16 +0100

    Actualización

--- src/segunda_evaluacion/tema06colecciones/poo/clasesyobjetos/cajero/Gestor.java
+++ src/segunda_evaluacion/tema06colecciones/poo/clasesyobjetos/cajero/Gestor.java
@@ -1,4 +1,8 @@
 package segunda_evaluacion.tema06colecciones.poo.clasesyobjetos.cajero;
 
 public class Gestor {
+
+    private String nombre;
+    private String telefono;
+    private double cantidadMax;
 }
