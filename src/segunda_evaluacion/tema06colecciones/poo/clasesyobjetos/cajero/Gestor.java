2c3d0e35f45a9ba6ec3932e8cbf880451b199fab
alcarazaina <alcarazdainara@gmail.com>
2024-01-31 17:58:42 +0100

    Actualización

--- src/segunda_evaluacion/tema06colecciones/poo/clasesyobjetos/cajero/Gestor.java
+++ src/segunda_evaluacion/tema06colecciones/poo/clasesyobjetos/cajero/Gestor.java
@@ -10,6 +10,8 @@ public class Gestor {
     public Gestor(String nombre, String telefono) {
         this.nombre = nombre;
         this.telefono = telefono;
+        // Si no se asigna el importe es 10000
+        this.cantidadMax = 10000;
     }
 
 
