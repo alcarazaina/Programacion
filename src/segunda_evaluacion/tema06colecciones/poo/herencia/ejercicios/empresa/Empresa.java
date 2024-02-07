02570aee06bf7f8cceb7c9bfa18eb59445d66750
alcarazaina <alcarazdainara@gmail.com>
2024-02-07 19:43:44 +0100

    Actualización

--- src/segunda_evaluacion/tema06colecciones/poo/herencia/ejercicios/empresa/Empresa.java
+++ src/segunda_evaluacion/tema06colecciones/poo/herencia/ejercicios/empresa/Empresa.java
@@ -1,4 +1,47 @@
 package segunda_evaluacion.tema06colecciones.poo.herencia.ejercicios.empresa;
 
+import java.util.ArrayList;
+
 public class Empresa {
+    private String nombre;
+    private ArrayList<Empleado> empleados;
+    private ArrayList<Cliente> clientes;
+
+    public Empresa(String nombre, ArrayList<Empleado> empleados, ArrayList<Cliente> clientes) {
+        this.nombre = nombre;
+        this.empleados = empleados;
+        this.clientes = clientes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public String mostrar() {
+        return "Empresa{" +
+                "nombre='" + nombre + '\'' +
+                ", empleados=" + empleados +
+                ", clientes=" + clientes +
+                '}';
+    }
 }
