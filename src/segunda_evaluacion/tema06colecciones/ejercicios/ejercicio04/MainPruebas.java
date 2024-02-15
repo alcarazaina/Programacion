9fee9577498536ca56c3110c3a846114f31d4c5a
alcarazaina <alcarazdainara@gmail.com>
2024-02-15 16:46:53 +0100

    Actualización

---
+++ src/segunda_evaluacion/tema06colecciones/ejercicios/ejercicio04/MainPruebas.java
@@ -0,0 +1,25 @@
+package segunda_evaluacion.tema06colecciones.ejercicios.ejercicio04;
+
+public class MainPruebas {
+    public static void main(String[] args) {
+
+
+        Diccionario miDiccionario = new Diccionario();
+        miDiccionario.nuevoPar("Perro", "Dog");
+        miDiccionario.nuevoPar("Gato", "Cat");
+        miDiccionario.nuevoPar("Mesa", "Table");
+        miDiccionario.nuevoPar("Silla", "Chair");
+        miDiccionario.nuevoPar("Programación", "Programming");
+        miDiccionario.nuevoPar("Pelo", "Hair");
+        miDiccionario.nuevoPar("Cara", "Face");
+        miDiccionario.nuevoPar("Labios", "Lips");
+
+        System.out.println(miDiccionario);
+        String traduccion = miDiccionario.traduce("Pelo");
+        System.out.println(traduccion);
+
+        miDiccionario.palabraAleatoria();
+        String primeraLetraTraducion = miDiccionario.primeraLetraTraducion("Gato");
+        System.out.println(primeraLetraTraducion);
+    }
+}
