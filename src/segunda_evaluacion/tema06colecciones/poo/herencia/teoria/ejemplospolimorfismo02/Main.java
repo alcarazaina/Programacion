1eb110df049bbe16b77e441d8f951b97627db202
alcarazaina <alcarazdainara@gmail.com>
2024-02-21 18:09:45 +0100

    Actualización

--- src/segunda_evaluacion/tema06colecciones/poo/herencia/teoria/ejemplospolimorfismo02/Main.java
+++ src/segunda_evaluacion/tema06colecciones/poo/herencia/teoria/ejemplospolimorfismo02/Main.java
@@ -3,5 +3,10 @@ package segunda_evaluacion.tema06colecciones.poo.herencia.teoria.ejemplospolimor
 public class Main {
     public static void main(String[] args) {
 
+        Fichero img = new Imagen("\\ruta\\al\\fichero\\img.png", 480, 640, new byte[]{4, 32});// aquí asignamos un objeto
+
+        img.imprimeInfoFichero();
+        // Imprime "Imagen: \ruta\al\fichero\img.png, width: 480, height: 640"
+
     }
 }
