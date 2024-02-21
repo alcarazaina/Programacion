af1ec1c952413f4afb870c23a01c846e0d2142b5
alcarazaina <alcarazdainara@gmail.com>
2024-02-21 17:58:12 +0100

    Actualización

--- src/segunda_evaluacion/tema06colecciones/poo/herencia/teoria/ejemplospolimorfismo02/Fichero.java
+++ src/segunda_evaluacion/tema06colecciones/poo/herencia/teoria/ejemplospolimorfismo02/Fichero.java
@@ -1,4 +1,23 @@
 package segunda_evaluacion.tema06colecciones.poo.herencia.teoria.ejemplospolimorfismo02;
 
 public class Fichero {
+    protected String nombreCompleto;
+
+    // constructor con un parámetro
+
+    public Fichero(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    // getters y setters
+
+    public void imprimeInfoFichero() {
+        String info = this.getInfoFichero(); // comportamiento polimórfico
+        System.out.println(info);
+    }
+
+    protected String getInfoFichero() {
+        return "Fichero: " + nombreCompleto;
+    }
+
 }
